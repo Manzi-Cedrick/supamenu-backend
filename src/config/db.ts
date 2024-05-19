db45bb566b5f215a1cd9d51652c914a679a95977
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-05-19 10:16:50 +0200

    [core]: project batch --update

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(
+    'supamenu_db',
+    'postgres',
+    'kabanda2005'
+    , {
+        host: 'localhost',
+        dialect: 'postgres',
+        logging: true,
+    })
+
+module.exports = sequelize 
